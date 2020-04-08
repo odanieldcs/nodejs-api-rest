1aeacfad63af5773dc20b926dac7f0f839061a80
Daniel Castro <danielcastro.rs@gmail.com>
2020-04-08 00:32:33 -0300

    :ok_hand: fix server app

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 (async () => {
   try {
     const app = await setup();
-    app.listen(port, () => console.log(`app running on port ${port}`));
+    app.listen(port, () => console.info(`app running on port ${port}`));
   } catch (error) {
     console.error(error);
     process.exit(1);
