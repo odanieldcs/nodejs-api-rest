9723bb1d5a7285058f63579e32b06144a5d36fcb
Daniel Castro <danielcastro.rs@gmail.com>
2020-04-13 16:04:43 -0300

    :white_check_mark: updated movie unit test

--- test/unit/controllers/movie.spec.js
+++ test/unit/controllers/movie.spec.js
@@ -1,3 +1,5 @@
+/* eslint-disable max-classes-per-file */
+/* eslint-disable class-methods-use-this */
 import sinon from 'sinon';
 import MovieController from '../../../src/controllers/movie';
 import Movie from '../../../src/models/movie';
@@ -48,6 +50,139 @@ describe('Controller: Movie', () => {
       await movieController.get(request, response);
 
       sinon.assert.calledWith(response.send, 'Error');
+    });
+  });
+
+  describe('getById()', () => {
+    it('should return a one movie', async () => {
+      const fakeId = 'fake-id';
+      const request = {
+        params: {
+          id: fakeId,
+        },
+      };
+      const response = {
+        send: sinon.spy(),
+      };
+
+      Movie.find = sinon.stub();
+      Movie.find.withArgs({ _id: fakeId }).resolves(defaultMovie);
+
+      const movieController = new MovieController(Movie);
+      await movieController.getById(request, response);
+
+      sinon.assert.calledWith(response.send, defaultMovie);
+    });
+
+    it('should return 400 when an error occurs', async () => {
+      const fakeId = 'fake-id';
+      const request = {
+        params: {
+          id: fakeId,
+        },
+      };
+      const response = {
+        send: sinon.spy(),
+        status: sinon.stub(),
+      };
+
+      response.status.withArgs(400).returns(response);
+
+      Movie.find = sinon.stub();
+      Movie.find.withArgs({ _id: fakeId }).rejects({ message: 'Error' });
+
+      const movieController = new MovieController(Movie);
+
+      await movieController.getById(request, response);
+
+      sinon.assert.calledWith(response.send, 'Error');
+    });
+  });
+
+  describe('create()', () => {
+    it('should save a new movie successfully', async () => {
+      const request = {
+        body: defaultMovie[0],
+        ...defaultMovie,
+      };
+      const response = {
+        send: sinon.spy(),
+        status: sinon.stub(),
+      };
+      class fakeMovie {
+        save() {}
+      }
+      response.status.withArgs(201).returns(response);
+      sinon.stub(fakeMovie.prototype, 'save').withArgs().resolves();
+
+      const movieController = new MovieController(fakeMovie);
+
+      await movieController.create(request, response);
+
+      sinon.assert.calledWith(response.send);
+    });
+  });
+
+  describe('update()', () => {
+    it('should respond with 200 when the movie has been updated', async () => {
+      const fakeId = 'fake-id';
+      const updatedMovie = {
+        _id: fakeId,
+        name: 'Updated movie',
+        description: 'Updated description',
+        year: 2020,
+      };
+      const request = {
+        params: {
+          id: fakeId,
+        },
+        body: updatedMovie,
+      };
+      const response = {
+        sendStatus: sinon.spy(),
+      };
+
+      class fakeMovie {
+        static updateOne() {}
+      }
+
+      const updateOneStub = sinon.stub(fakeMovie, 'updateOne');
+
+      updateOneStub
+        .withArgs({ _id: fakeId }, updatedMovie)
+        .resolves(updatedMovie);
+      const movieController = new MovieController(fakeMovie);
+
+      await movieController.update(request, response);
+      sinon.assert.calledWith(response.sendStatus, 200);
+    });
+  });
+
+  describe('delete()', () => {
+    it('should respond with 204 when the movie has been deleted', async () => {
+      const fakeId = 'fake-id';
+      const request = {
+        params: {
+          id: fakeId,
+        },
+      };
+      const response = {
+        sendStatus: sinon.spy(),
+      };
+
+      class fakeMovie {
+        static deleteOne() {}
+      }
+
+      const deleteOneStub = sinon.stub(fakeMovie, 'deleteOne');
+
+      deleteOneStub.withArgs({ _id: fakeId }).resolves();
+
+      const movieController = new MovieController(fakeMovie);
+
+      await movieController.delete(request, response);
+
+      sinon.assert.calledWith(response.sendStatus, 204);
     });
   });
 });
