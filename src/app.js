0fb9f6062e1e141529cf71dbf52b5556cb1cf8e6
Daniel Castro <danielcastro.rs@gmail.com>
2020-04-23 21:55:53 -0300

    :construction: app function converted to class es6

--- src/app.js
+++ src/app.js
@@ -1,30 +1,29 @@
 import './env';
+import './database';
 import express from 'express';
 import bodyParser from 'body-parser';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import routes from './routes';
 import { loggerMiddleware } from './middlewares/logger';
-import database from './database';
 
-const prepare = () => {
-  const app = express();
+class App {
+  constructor() {
+    this.server = express();
+    this.middlewares();
+    this.routes();
+  }
 
-  app.use(bodyParser.json());
-  app.use(morgan('combined'));
-  app.use(helmet());
-  app.use(loggerMiddleware);
+  middlewares() {
+    this.server.use(bodyParser.json());
+    this.server.use(morgan('combined'));
+    this.server.use(helmet());
+    this.server.use(loggerMiddleware);
+  }
 
-  app.use('/', routes);
+  routes() {
+    this.server.use(routes);
+  }
+}
 
-  app.database = database;
-
-  return app;
-};
-
-export default async () => {
-  const app = prepare();
-  await app.database.connect();
-
-  return app;
-};
+export default new App().server;
--- src/server.js
+++ src/server.js
@@ -1,13 +1,10 @@
-import setup from './app';
+import app from './app';
 
 const port = process.env.PORT || 3000;
 
-(async () => {
-  try {
-    const app = await setup();
-    app.listen(port, () => console.info(`Listening on port ${port}`));
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
-  }
-})();
+try {
+  app.listen(port, () => console.info(`Listening on port ${port}`));
+} catch (error) {
+  console.error(error);
+  process.exit(1);
+}
